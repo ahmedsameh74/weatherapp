433009bdff523ce08af70fc06a42a400021eb6e8
ahmed sameh <asameh1500@gmail.com>
2022-02-26 17:18:18 +0200

    deploy

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,11 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
-
-
+const server = new Server();
+// console.log(Server)
 const updateUi = (data) => {
     // console.log(data)
     const {cityDetails, weather} = data;
-
 
     details.innerHTML = `
                 <h5 class="my-5">${cityDetails.EnglishName}</h5>
@@ -23,11 +22,6 @@ const updateUi = (data) => {
     icon.setAttribute('src', iconSrc);
 
     let src = weather.IsDayTime ? 'images/day.svg' : 'images/night.svg';
-    // if(weather.IsDayTime){
-    //     src = 'images/day.svg';
-    // }else{
-    //     src = 'images/night.svg';
-    // }
     time.setAttribute('src', src);
 
 
@@ -38,21 +32,12 @@ const updateUi = (data) => {
 }
 
 
-const updateCity = async (city) => {
-    const cityDetails = await getCity(city);
-    const weather = await getWeather(cityDetails.Key);
-    return {
-        cityDetails,
-        weather
-    };
-}
-
 cityForm.addEventListener('submit', e => {
     e.preventDefault();
     // console.log(cityForm.city.value);
     const city = cityForm.city.value.trim();
     cityForm.reset();
-    updateCity(city)
+    server.updateCity(city)
     .then(data => updateUi(data))
     .catch(err => console.log(err));
 
@@ -60,7 +45,7 @@ cityForm.addEventListener('submit', e => {
 })
 
 if(localStorage.getItem('city')){
-    updateCity(localStorage.getItem('city'))
+    server.updateCity(localStorage.getItem('city'))
     .then(data => updateUi(data))
     .catch(err => console.log(err));
 };
--- scripts/server.js
+++ scripts/server.js
@@ -1,30 +1,31 @@
-const key = 'NcpRjHcyWsteOi2aMHLMSYW6tA3XT5IO';
-
-const getWeather = async (id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
-    const query = `${id}?apikey=${key}`;
-    const response = await fetch(base + query);
-    const data = await response.json();
-    // console.log(data);
-    return data[0];
+class Server{
+    constructor(){
+        this.key = 'NcpRjHcyWsteOi2aMHLMSYW6tA3XT5IO';
+        this.weatherUri = 'https://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityUri = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city){
+        const cityDetails = await this.getCity(city);
+        const weather = await this.getWeather(cityDetails.Key);
+           return {
+             cityDetails,
+             weather
+            };
+    }
+    async getCity(city){
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityUri + query);
+        const data = await response.json();
+    
+        return data[0];
+    }
+    async getWeather(id){
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherUri + query);
+        const data = await response.json();
+        // console.log(data);
+        return data[0];
+    }
 }
 
 
-
-
-
-
-const getCity = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
-};
-
-// getCity('cairo')
-// .then(data => {
-//     return getWeather(data.Key);
-// }).then(data => console.log(data))
-// .catch(err => console.log(err));
